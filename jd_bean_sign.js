0ef43b4dc6b09c8aaee820a2331fd1bca8d8223d
esiwsh <esiwsh@163.com>
2021-06-07 13:41:32 +0800

    更新

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -14,7 +14,6 @@ const $ = new Env('京东多合一签到');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 const exec = require('child_process').execSync
 const fs = require('fs')
 const download = require('download');
