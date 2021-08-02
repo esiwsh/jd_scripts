cadca125894b9a93567f06e8dafc3433d228843d
esiwsh <esiwsh@163.com>
2021-08-02 23:05:03 +0800

    更新

--- jd_qqxing.js
+++ jd_qqxing.js
@@ -32,7 +32,7 @@ if ($.isNode()) {
 
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
 message = ""
-$.shareuuid = "0e3a1d7b4ef042aeab07e16d5ad76793" //
+$.shareuuid = "e963cb6a72b94c35b12a4ce9ab0ca0ea" //
     !(async () => {
         if (!cookiesArr[0]) {
             $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {
