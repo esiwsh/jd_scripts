f08ab289144d4f95b36daaffac67df57272a3d90
esiwsh <esiwsh@163.com>
2021-09-24 23:21:17 +0800

    更新

--- utils/JD_DailyBonus.js
+++ utils/JD_DailyBonus.js
@@ -13,7 +13,7 @@ var Key = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用jso
 
 var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用json串数据  ↓
 
-var OtherKey = ``; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
+var OtherKey = `[{"cookie":"pt_key=AAJhPaFqADCpjmMD81OfXa4320n_OQTVEWY_3zGFyWDfgTtxQS7qQQzGcjs-dBjsE8ZTgur_efQ; pt_pin=jd_40f8e5665fc75;","jrBody":""}]`; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 
 var LogDetails = false; //是否开启响应日志, true则开启
